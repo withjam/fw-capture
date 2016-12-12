abde6d9d56200839cc73f8c5edb4adf5035c7493
Matt Pileggi <Matt.Pileggi@MarkLogic.com>
2016-12-12 08:56:44 -0500

    use plugin settings

--- fw-campaigns.php
+++ fw-campaigns.php
@@ -102,166 +102,87 @@
   }
 
   function fw_campaign_footer() {
+    // utm capture code
+    $disclaimer = get_option('fw_capture_disclaimer');
+    $btnLabel = get_option('fw_capture_button_label', 'Sign Up');
+    $interDelay = get_option('fw_capture_disclaimer');
+    $interTitle = get_option('fw_capture_button_label', 'Join Our Mailing List!');
+    $plugin_footer = <<< EOT
+    <style>body.noscroll{overflow:hidden}#fw-overlay-modal{z-index:10000;position:fixed;width:100%;height:100%;top:0;left:0;opacity:0;overflow:auto;transition:opacity .5s linear;background-color:rgba(0,0,0,.85);text-align:center}#fw-overlay-modal form{display:inline-block;position:relative;top:10%;min-width:25%;min-height:80%;overflow:visible;background:#FFF;margin-bottom:10%}#fw-overlay-modal p{ margin: 20px 0;line-height: 1.55em;}#fw-overlay-modal h4{ margin: 15px 0 }#fw-overlay-modal p.small{line-height:1.25em;font-size:0.85em}#fw-overlay-modal .form-group{margin:15px}#fw-overlay-modal label{color:#40A8C5}#fw-overlay-modal.fadeout{opacity:0}#fw-overlay-modal.fadein{opacity:1}#fw-overlay-modal .fw-overlay-close{display:inline-block;-webkit-box-sizing:content-box;-moz-box-sizing:content-box;box-sizing:content-box;width:.4em;height:.4em;position:absolute;top:-25px;right:-25px;border:none;-webkit-border-radius:1em;border-radius:1em;font:400 8em/normal Arial,Helvetica,sans-serif;color:rgba(0,0,0,1);-o-text-overflow:clip;text-overflow:clip;background:#40A8C5;cursor:pointer}#fw-overlay-modal .fw-overlay-close:after,#fw-overlay-modal .fw-overlay-close:before{display:inline-block;width:.25em;height:.075em;position:absolute;content:"";top:.165em;left:.075em;border:none;font:400 100%/normal Arial,Helvetica,sans-serif;color:rgba(0,0,0,1);-o-text-overflow:clip;text-overflow:clip;background:#fff;text-shadow:none}#fw-overlay-modal .fw-overlay-close:before{-webkit-box-sizing:content-box;-moz-box-sizing:content-box;box-sizing:content-box;-webkit-transform:rotateZ(45deg);transform:rotateZ(45deg)}#fw-overlay-modal .fw-overlay-close:after{-webkit-box-sizing:content-box;-moz-box-sizing:content-box;box-sizing:content-box;-webkit-transform:rotateZ(-45deg);transform:rotateZ(-45deg)}#fw-overlay-modal.form-error input{border-color:red;background-color:#FEE}#fw-overlay-modal .error-message{display:none;color:red;font-size:.875em}#fw-overlay-modal.form-error .error-message{display:block}</style>
+    <script>
+    // fw utm campaign script
+    (function() {
+      // regex for validating email format
+      var email_pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/g;
+
+      var fw_capture = {
+        btnLabel: '{$btnLabel}',
+        disclaimer: '${disclaimer}'
+      };
+
+      fw_capture.interstitial = {
+        label: '{$interTitle}',
+        delay: '{$interDelay}'
+      }
+
+      fw_capture.openModal = function(offerData, width) {
+        // prevent regular scrolling
+        jQuery('body').addClass('noscroll');
+
+        fw_capture.overlay = document.createElement('DIV');
+        fw_capture.overlay.id = 'fw-overlay-modal';
+
+        var form = '<form id="optin-form" style="width: ' + offerData.width + 'px;"><a class="fw-overlay-close"></a><div id="fw-overlay-modal-content-wrapper"><h4>' + offerData.title + '</h4><img src="' + offerData.img +'"><p>' + offerData.desc + '</p><input type="hidden" name="fw_capture_offer_url" value="' + offerData.offer_url + '"></div><div class="overlay-capture-form"><div class="form-group"><label for="fw-campaign-email-input">Enter Your Email Address</label> <div class="error-message">Please enter a valid email</div><input id="fw-campaign-email-input" type="email" class="form-control" placeholder="your@email.com"></div><div class="form-group"><a class="btn btn-primary fw-overlay-submit">' + ( fw_capture.btnLabel || "Sign Up" )+ '</a><p class="small"><em>' + fw_capture.disclaimer + '</em></p></div></div></form>';
+        
+        fw_capture.overlay.innerHTML = form;
+
+        document.body.append(fw_capture.overlay);
+
+        fw_capture.activeOverlay = jQuery(document.getElementById('fw-overlay-modal')).on('click', fw_capture.handleClick);
+        window.setTimeout(function() { fw_capture.activeOverlay.addClass('fadein'); }, 100);
+      }
+
+      fw_capture.closeModal = function() {
+        // enable scrolling again
+        jQuery('body').removeClass('noscroll');
+        fw_capture.activeOverlay.removeClass('fadein').addClass('fadeout');
+        window.setTimeout(function() {
+          document.body.removeChild(fw_capture.activeOverlay.get(0));
+          delete fw_capture.activeOverlay;
+        }, 500);
+      }
+
+      fw_capture.handleClick = function(evt) {
+        var target = jQuery(this);;
+        if (target.is('fw-overlay-cancel')) {
+          fw_capture.closeModal();
+        } else if (target.is('fw-overlay-submit')) {
+          var email = document.getElementById('fw-campaign-email-input').value;
+          fw_capture.activeOverlay.removeClass('form-error');
+          if (!email_pattern.test(email)) {
+            fw_capture.activeOverlay.addClass('form-error');
+          } else {
+            // show thank you message
+            fw_capture.activeOverlay.find('.overlay-capture-form').html('<div class="form-group"><h4>Thank you!</h4><a class="btn btn-default fw-overlay-cancel">Done</a></div>');
+          }
+        }
+      }
+
+      window.fw_capture = fw_capture;
+    })(window)
+    </script>
+EOT;
+    echo $plugin_footer; 
+    // utm campaign specific
     $code = $_GET['utm_campaign'];
     if (isset($code)) {
       $codeData = get_option('fw_capture_utm_code_' . $code);
       if ($codeData) {
-        $utmData = json_decode($codeData);
-        $utmTitle = $utmData->title;
-        $utmDesc = $utmData->desc;
-        $utmImg = $utmData->img;
-        $utmWidth = $utmData->width;
-        $utmOfferUrl = $utmData->offer_url;
         $html = <<<EOT
-        <style>
-        body.noscroll { overflow: hidden; }
-        #fw-overlay-modal { z-index: 10000; position: fixed; width: 100%; height: 100%; top: 0; left: 0; opacity: 0; overflow: auto; transition: opacity 0.5s linear; background-color: rgba(0,0,0,0.85); text-align: center;}
-        #fw-overlay-modal form { display: inline-block; position: relative; top: 10%; min-width: 25%; min-height: 80%; overflow: visible; background: #FFF; margin-bottom: 10%; }
-        #fw-overlay-modal .form-group { margin: 15px;}
-        #fw-overlay-modal label { color: #40A8C5; }
-        #fw-overlay-modal.fadeout { opacity: 0 }
-        #fw-overlay-modal.fadein { opacity: 1 }
-        #fw-overlay-modal .fw-overlay-close {
-          display: inline-block;
-          -webkit-box-sizing: content-box;
-          -moz-box-sizing: content-box;
-          box-sizing: content-box;
-          width: 0.4em;
-          height: 0.4em;
-          position: absolute;
-          top: -25px;
-          right: -25px;
-          border: none;
-          -webkit-border-radius: 1em;
-          border-radius: 1em;
-          font: normal 8em/normal Arial, Helvetica, sans-serif;
-          color: rgba(0,0,0,1);
-          -o-text-overflow: clip;
-          text-overflow: clip;
-          background: #40A8C5;
-          cursor: pointer;
-        }
-
-        #fw-overlay-modal .fw-overlay-close:before {
-          display: inline-block;
-          -webkit-box-sizing: content-box;
-          -moz-box-sizing: content-box;
-          box-sizing: content-box;
-          width: 0.25em;
-          height: 0.075em;
-          position: absolute;
-          content: "";
-          top: 0.165em;
-          left: 0.075em;
-          border: none;
-          font: normal 100%/normal Arial, Helvetica, sans-serif;
-          color: rgba(0,0,0,1);
-          -o-text-overflow: clip;
-          text-overflow: clip;
-          background: #ffffff;
-          text-shadow: none;
-          -webkit-transform: rotateZ(45deg)   ;
-          transform: rotateZ(45deg)   ;
-        }
-
-        #fw-overlay-modal .fw-overlay-close:after {
-          display: inline-block;
-          -webkit-box-sizing: content-box;
-          -moz-box-sizing: content-box;
-          box-sizing: content-box;
-          width: 0.25em;
-          height: 0.075em;
-          position: absolute;
-          content: "";
-          top: 0.165em;
-          left: 0.075em;
-          border: none;
-          font: normal 100%/normal Arial, Helvetica, sans-serif;
-          color: rgba(0,0,0,1);
-          -o-text-overflow: clip;
-          text-overflow: clip;
-          background: #ffffff;
-          text-shadow: none;
-          -webkit-transform: rotateZ(-45deg)   ;
-          transform: rotateZ(-45deg)   ;
-        }
-
-        #fw-overlay-modal.form-error input { border-color: red; background-color: #FEE; }
-        #fw-overlay-modal .error-message { display: none; color: red; font-size: 0.875em; }
-        #fw-overlay-modal.form-error .error-message { display: block; }
-        </style>
         <script>
-        // campaign code
-        (function(window, document) {
-            var email_pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/g;
-          function getQS(q) {
-            return function(r) {
-              if ("" == r) return {};
-              for (var e = {}, n = 0; n < r.length; ++n) {
-                var t = r[n].split("=");
-                if (2 == t.length) {
-                  var a = t[0],
-                    i = decodeURIComponent(t[1].replace(/\+/g, " "));
-                  e[a] && !Array.isArray(e[a]) ? (e[a] = [e[a]], e[a].push(i)) : e[a] = i
-                }
-              }
-              return e
-            }(q.split("&"));
-          };
-
-          var params = getQS(document.location.search.slice(1));
-
-          var utm_code = params.utm_campaign;
-
-          if (utm_code) {
-            jQuery('body').addClass('noscroll');
-            var ts = new Date().getTime();
-            var overlay = document.createElement('DIV');
-            overlay.id = 'fw-overlay-modal';
-            var form = '<form id="optin-form" style="width: {$utmWidth}px;"><a class="fw-overlay-close"></a><div id="fw-overlay-modal-content-wrapper"><h4>{$utmTitle}</h4><img src="{$utmImg}"><p>{$utmDesc}</p><input type="hidden" name="fw_capture_offer_url" value="{$utmOfferUrl}"></div><div class="overlay-capture-form"><div class="form-group"><label for="fw-campaign-email-input">Enter Your Email Address</label> <div class="error-message">Please enter a valid email</div> <input id="fw-campaign-email-input" type="email" class="form-control" placeholder="your@email.com"></div><div class="form-group"><a class="btn btn-primary fw-overlay-submit">Sign Up</a></div></div></form>';
-            overlay.innerHTML = form;
-            document.body.append(overlay);
-            overlay = document.getElementById('fw-overlay-modal');
-            try {
-              overlay.addEventListener('click', handleClick, false);
-            } catch(ex) {
-                overlay.attachEvent('onclick', handleClick);
-            }
-            
-            window.setTimeout(function() {
-              console.log('fadein');
-              overlay.className = 'fadein';
-            }, 500);
-            
-            function handleClick(event) {
-               var e = event || window.event;
-                     var target = e.target;
-                     switch(target.className) {
-                case 'fw-overlay-close':
-                case 'btn btn-default fw-overlay-cancel':
-                 console.log('fadeout');
-                  overlay.className = 'fadeout';
-                  window.setTimeout(function() {
-                    document.body.removeChild(overlay);
-                    delete overlay;
-                  }, 500);
-                  jQuery('body').removeClass('noscroll');
-                  break;
-                case 'btn btn-primary fw-overlay-submit':
-                  var email = document.getElementById('fw-campaign-email-input').value;
-                  overlay.className = 'fadein';
-                  if (!email_pattern.test(email)) {
-                    overlay.className = 'fadein form-error';
-                  } else {
-                    jQuery(overlay).find('.overlay-capture-form').html('<div class="form-group"><h4>Thank you!</h4><a class="btn btn-default fw-overlay-cancel">Done</a></div>');
-                  }
-
-                  break;
-               }
-            }
-          }
-
-        })(window, document)
+        (function() {
+          fw_capture.openModal(JSON.parse('{$codeData}'));
+        })()
         </script>
 EOT;
 
